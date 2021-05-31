c4d02c3776ebf21a9815ab7ea14101d5af6e9236
shawnxys789 <shawnxys789@outlook.com>
2021-05-31 13:57:05 +0800

    Update util.py

--- util.py
+++ util.py
@@ -10,7 +10,7 @@ def save_adjacency_matrix_in_csv(file_name, adjacency_matrix, variable_names):
     W_est_full_csv = np.array(np.zeros((adjacency_matrix.shape[0] + 1, adjacency_matrix.shape[1] + 1)), dtype=object)
 
     W_est_full_csv[1:, 1:] = adjacency_matrix  # copy adjacency matrix
-    W_est_full_csv[0, 0] = 'row->column'
+    W_est_full_csv[0, 0] = 'column->row'
     W_est_full_csv[0, 1:] = variable_names  # set column names
     W_est_full_csv[1:, 0] = variable_names  # set row names
 
@@ -20,7 +20,7 @@ def save_adjacency_matrix_in_csv(file_name, adjacency_matrix, variable_names):
 def draw_DAGs_using_LINGAM(file_name, adjacency_matrix, variable_names):
     lower_limit = 0.0
 
-    dot = make_dot(np.transpose(adjacency_matrix), labels=variable_names, lower_limit=lower_limit)
+    dot = make_dot(adjacency_matrix, labels=variable_names, lower_limit=lower_limit)
 
     dot.format = 'png'
     dot.render(file_name)
